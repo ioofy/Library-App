04481928aeed8c53487f60ca1aeab5b4561fce57
ioofy <mynev.id@gmail.com>
2022-03-22 01:44:08 +0800

    feat: .

--- src/pages/HomePages/HomePages.tsx
+++ src/pages/HomePages/HomePages.tsx
@@ -1,12 +1,21 @@
+import { useAppSelector } from 'hooks/useGetData';
 import TopHeader from 'layout/components/TopHeader';
 
 const HomePages = () => {
+  const {
+    data: {
+      data: { access_token },
+    },
+  } = useAppSelector(
+    (state) => state.persistedReducer.auth
+  );
   return (
     <div>
       <TopHeader />
       <h1 className='text-4xl text-center mt-24'>
-        👋 Hello, Please Login To See The
-        Dashboard
+        {access_token
+          ? '👋 Hi, You are logged in'
+          : '👋 Hello, Please Login To See The Dashboard'}
       </h1>
     </div>
   );
