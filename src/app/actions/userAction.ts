0f899356318fe031dc4ac9b75e24da55233fae52
ioofy <mynev.id@gmail.com>
2022-05-30 21:00:51 +0700

    chore: remove get user pending

--- src/app/actions/userAction.ts
+++ src/app/actions/userAction.ts
@@ -1,16 +1,9 @@
-import {
-  getUserFail,
-  getUserPending,
-  getUserSuccess,
-} from 'app/slice/userSlice';
-
+import { getUserFail, getUserSuccess } from 'app/slice/userSlice';
 import { getUser } from 'api/userApi';
 import { UserProps } from 'types/declare';
 
 export const getMyProfile = () => async (dispatch: any) => {
   try {
-    dispatch(getUserPending());
-
     // call API
     const responseUser: UserProps = await getUser();
 
